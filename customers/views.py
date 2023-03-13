a0eafff03bc78025a52ed04c1b68a82fac04a79b
Lena <elfiora2@icloud.com>
2023-03-12 19:56:51 -0700

    Corrections

--- customers/views.py
+++ customers/views.py
@@ -9,9 +9,10 @@ from rest_framework.views import APIView
 import uuid
 from .models import Customer 
 from django.contrib.auth import get_user_model
+User = get_user_model()
 from rest_framework.response import Response
 from rest_framework import status
-User = get_user_model()
+
 
 class CustomerCreate(APIView):
     http_method_names = ['post',]
@@ -22,17 +23,13 @@ class CustomerCreate(APIView):
         print("AGENT:", client_agent)
         customer_token = str(uuid.uuid4())
         Customer.objects.create(token=customer_token)
-        responce = {
+        response = {
             "status": True,
             "customer_token": customer_token,
             }
-        return Response(data=responce, status=status.HTTP_201_CREATED)
-    
-"""
-???? вот тут меня терзают смутные сомнения: у нас в  сore > urls.py написано 2 views: TokenObtainPairView и TokenRefreshView. А в
-документации Postman их сразу два в responce.  Я тут, на всякий случай сделала 2 сериалайзера и 2view. Но что-то как-то
-здесь не так... 
-"""  
+        return Response(data=response, status=status.HTTP_201_CREATED)
+
+
 
 class TokenObtainPairView(TokenObtainPairView):
     serializer_class = UserTokenObtaiPairSerializer
@@ -42,24 +39,25 @@ class TokenObtainPairView(TokenObtainPairView):
         serializer_class = self.serializer_class(data=request.data)
         serializer_class.is_valid(raise_exception=True)
         serializer_class.save()
-        responce = {
+        response = {
             'access': str(JWTAuthentication.get_validated_token),
+            'refresh': str(JWTAuthentication.get_validated_token),
         } 
-        return Response(data=responce, status=status.HTTP_201_CREATED)
+        return Response(data=response, status=status.HTTP_201_CREATED)
 
 
-class TokenRefreshView(TokenRefreshView):
-    serializer_class = UserTokenRefreshSerializer
-    authentication_classes = (JWTAuthentication)
-    permission_classes = (IsAuthenticated,)
-    def post(self, request, *args, **kwargs):
-        serializer_class = self.serializer_class(data=request.data)
-        serializer_class.is_valid(raise_exception=True)
-        serializer_class.save()
-        responce = {
-            'access': str(JWTAuthentication.get_validated_token),
-        } 
-        return Response(data=responce, status=status.HTTP_201_CREATED)
+# class TokenRefreshView(TokenRefreshView):
+#     serializer_class = UserTokenRefreshSerializer
+#     authentication_classes = (JWTAuthentication)
+#     permission_classes = (IsAuthenticated,)
+#     def post(self, request, *args, **kwargs):
+#         serializer_class = self.serializer_class(data=request.data)
+#         serializer_class.is_valid(raise_exception=True)
+#         serializer_class.save()
+#         response = {
+#             'refresh': str(JWTAuthentication.get_validated_token),
+#         } 
+#         return Response(data=response, status=status.HTTP_201_CREATED)
 
 
 class CustomerRegistration(APIView):
@@ -74,7 +72,7 @@ class CustomerRegistration(APIView):
             last_name = Customer.objects.get_or_create(last_name=self.request.data['last_name'])
             email = Customer.objects.get_or_create(email=self.request.data['email'])
             password = Customer.objects.get(password=self.request.user['password'])
-            responce = {
+            response = {
                 'username': username,
                 'first_name': first_name,
                 'last_name': last_name,
@@ -82,13 +80,13 @@ class CustomerRegistration(APIView):
                 'password': password,
                 'token': token
                 }
-            return Response(data=responce, status=status.HTTP_200_OK) 
+            return Response(data=response, status=status.HTTP_200_OK) 
         except Customer.DoesNotExist:
-                responce = {
+                response = {
                     'status': False,
                     'error': 'Customer does not exist'
                 }
-                return Response(data=responce, status=status.HTTP_400_BAD_REQUEST)
+                return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GetAuthCustomer(generics.RetrieveAPIView):
--- orders/views.py
+++ orders/views.py
@@ -15,27 +15,27 @@ class UpdateCart(APIView):
             try:
                 customer = Customer.objects.get(token=self.request.data['token'])#GET CUSTOMER BY TOKEN iF EXISTS
             except Customer.DoesNotExist:
-                responce = {
+                response = {
                     'status': False,
                     'error': 'Customer does not exist'
                 }
-                return Response(data=responce, status=status.HTTP_400_BAD_REQUEST)
+                return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
 
             try:
                 product = Product.objects.get(pk=self.request.data['product_id'])#GET PRODUCT BY ID IF EXISTS
             except Product.DoesNotExist:
-                responce = {
+                response = {
                     'status': False,
                     'error': 'Product does not exist'
                 }
-                return Response(data=responce, status=status.HTTP_400_BAD_REQUEST) 
+                return Response(data=response, status=status.HTTP_400_BAD_REQUEST) 
 
             if product.quantity < self.request.data['quantity']:
-                responce ={
+                response ={
                     'status': False,
                     'error': 'Last item left'#DEFAULT QUANTITY==1, NOT NULL, NOT BLANK
                 }
-                return  Response(data=responce, status=status.HTTP_400_BAD_REQUEST)
+                return  Response(data=response, status=status.HTTP_400_BAD_REQUEST)
 
             orders = Order.objects.filter(customer=customer, is_ordered=False).order_by('-id')#also  could be filtered by "-time_created" (DESC)
             if orders.count() == 0:
@@ -62,7 +62,7 @@ class UpdateCart(APIView):
             count_items = 0
             for item in products_in_order:
                 count_items += item['quantity']
-            responce = {
+            response = {
                 'status': True,
                 'cart_items_count': count_items    
             }# RESPONCE FOR CART UPDATE!
@@ -70,14 +70,14 @@ class UpdateCart(APIView):
             product.quantity = product.quantity - self.request.data['quantity']#QUANTITY OF PRODUCTS LEFT AFTER THIS ORDER
             product.save()
 
-            return Response(data=responce, status=status.HTTP_200_OK) 
+            return Response(data=response, status=status.HTTP_200_OK) 
          
         except BaseException as error:
-            responce ={
+            response ={
                 'status': False,
                 'error': str(error)
             } 
-            return Response(data=responce, status=status.HTTP_200_OK)
+            return Response(data=response, status=status.HTTP_200_OK)
 
 
 class CartList(generics.ListAPIView):
@@ -96,19 +96,19 @@ class FinalizeOrder(APIView):
     serializer_class= OrderSerializer#хочется знать, уместно ли тут это?
     queryset = Order.objects.all() 
     http_method_names = ['put',]
-    def finalize_order_set(self, request, responce, *args, **kwargs):
+    def finalize_order_set(self, *args, **kwargs):
         try: #????нам ведь нужен именно покупатель, у которого есть товар в корзине, так? 
             try: #Retrieving: order_product__customer  
-                customer = OrderProduct.objects.filter(#
-                    order__customer=self.request.data['order__customer'],
+                customer = OrderProduct.objects.filter(
+                    order__customer=self.request.data['id'],                    
                     order_product__is_ordered=False
                     )
                 token = OrderProduct.objects.filter(# retrieving order_product__customer__token 
-                    order__customer__token=self.request.data['order__customer__token'], 
+                    order__customer__token=self.request.data['token'], 
                     order__is_ordered=False
                     )
             except customer is None:
-                responce = {
+                response = {
                     'status': False,
                     'error': 'Customer does not exist'
                 }
@@ -119,42 +119,27 @@ class FinalizeOrder(APIView):
                     order =self.request.data['order'], 
                     order__customer=customer, 
                     order__is_ordered=False
-                    )
+                )
             except order is None:
-                responce = {
+                response = {
                     'status': False,
                     'error': 'Customer has no active orders at the moment'
                 }
-                return Response(data=responce, status=status.HTTP_400_BAD_REQUEST)
-            try:
+                return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+            
             #Rertrieving customer data using Customer  Model  and saving them
-                customer.first_name = Customer.objects.get_or_create(first_name=self.request.data['first_name'])
-                customer.last_name = Customer.objects.get_or_create(last_name=self.request.data['last_name'])
-                customer.email = Customer.objects.get_or_create(email=self.request.data['email'])
-                customer.phone = Customer.objects.get_or_create(phone=self.request.data['phone'])
-                customer.address = Customer.objects.get_or_create(address=self.request.data['address'])
-                customer.save()
-            except Customer.DoesNotExist:
-                responce ={
-                'status': False,
-                'error': 'Customer does not exist'
-                } 
-                return Response(data=responce, status=status.HTTP_400_BAD_REQUEST)
-                    
-            try:
-                ##Rertrieving CustomerAddress data using CustomerAddress Model and saving them
-                customer.address_city = CustomerAddress.objects.filter(city=self.request.data['city'], customer=customer)
-                customer.address_post_code = CustomerAddress.objects.filter(post_code=self.request.data['post_code'], customer=customer)
-                customer.address_country = CustomerAddress.objects.filter(country=self.request.data['country'], customer=customer)
-                customer.address_address =CustomerAddress.objects.filter(customer_address=self.request.data['customer_address'], customer=customer)
-                customer.customer_address.save()
-            except CustomerAddress.DoesNotExist:
-                responce ={
-                'status': False,
-                'error': 'Customer address does not exist'
-                }
-                return Response(data=responce, status=status.HTTP_400_BAD_REQUEST) 
-            #Creating request       
+            customer.first_name = Customer.objects.get_or_create(first_name=self.request.data['first_name'])
+            customer.last_name = Customer.objects.get_or_create(last_name=self.request.data['last_name'])
+            customer.email = Customer.objects.get_or_create(email=self.request.data['email'])
+            customer.phone = Customer.objects.get_or_create(phone=self.request.data['phone'])
+            customer.address = Customer.objects.get_or_create(address=self.request.data['address'])
+            customer.save() 
+            #Rertrieving CustomerAddress data using CustomerAddress Model and saving them
+            customer.address_city = CustomerAddress.objects.filter(city=self.request.data['city'], customer=customer)
+            customer.address_post_code = CustomerAddress.objects.filter(post_code=self.request.data['post_code'], customer=customer)
+            customer.address_country = CustomerAddress.objects.filter(country=self.request.data['country'], customer=customer)
+            customer.address_address =CustomerAddress.objects.filter(customer_address=self.request.data['customer_address'], customer=customer)
+            customer.customer_address.save()
             request = {
                 'first_name': customer.first_name,
                 'last_name': customer.last_name,
@@ -176,8 +161,8 @@ class FinalizeOrder(APIView):
                 customer_shipping_address= order_serializer.validated_data['customer_shipping_address']
                 is_ordered = order_serializer.validated_data['True']
                 order_serializer.save()
-            #Creating final(main) responce:??? может, здесь надо  try: finally:?
-            responce = {
+            #Creating final responce:?
+            response = {
                 'id': order,
                 'time-created': time_created,
                 'time_checkout': time_checkout,
@@ -186,25 +171,13 @@ class FinalizeOrder(APIView):
                 'customer': customer,
                 'customer_shipping_address': customer_shipping_address    
                 }
-            return Response(data=responce, status=status.HTTP_200_OK)
+            return Response(data=response, status=status.HTTP_200_OK)
         except BaseException as error:
             responce ={
             'status': False,
             'error': 'No customer matching this order'
             }
-            return Response(data=responce, status=status.HTTP_400_BAD_REQUEST)
-            
-
-
-
-
-
-            
-
-
-
-
-   
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
         
 
 
